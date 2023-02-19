a446a1258f61e78f2420d91a49e87edff1736aa5
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2023-02-19 14:31:57 -0500

    fixed files and added button test

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,29 +1,16 @@
 import React from "react";
-import logo from "../assets/logo.svg";
 import "../styles/App.css";
 
-import {Button} from "@mui/material";
+import {ENGButton} from "../components/atoms";
 
 function App() {
   const buttonHandler = () => {
-    console.log("le picaron");
+    console.log("le picaron ");
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <h4>Test of MUI library</h4>
-        <Button color="secondary" onClick={buttonHandler} variant="contained">
-          Contained
-        </Button>
-      </header>
+      <ENGButton variant="contained" onClick={buttonHandler}></ENGButton>
     </div>
   );
 }
