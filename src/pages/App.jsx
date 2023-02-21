8d9497c56c5fd9ad6462ab394c28cff556a8ce3b
LAPTOP-L3MGEOQK\ascal <ascalvete@hotmail.com>
2023-02-21 14:28:27 -0500

    Merge

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,34 +1,6 @@
 import React from "react";
 import "../styles/App.css";
 
-<<<<<<< HEAD
-import {ENGTable} from "../components/atoms";
-
-function App() {
-  return (
-    <div className="App">
-      <ENGTable 
-        /*tableWidth={800} 
-        tableAlignment="left" */
-        cellAlignment="left"
-        cellVerticalAlignment="bottom"
-        columnTitles = {[
-          "Num",
-          <input type={"checkbox"}></input>,
-          "Nombre",
-          "Edad",
-          "Altura"
-        ]}
-        rowsContent={
-          [
-            [1,<input type={"checkbox"}></input>,"Andres", 26, 1.73],
-            [2, <input type={"checkbox"}></input>, "Jorge"],
-            [3, <input type={"checkbox"}></input>, "Juana", 20, 1.70]
-          ]
-        }    
-      >
-      </ENGTable>      
-=======
 import {ENGButton, ENGCheckbox} from "../components/atoms";
 
 function App() {
@@ -43,7 +15,6 @@ function App() {
         CLICK
       </ENGButton>
       <ENGCheckbox checked={true}/>
->>>>>>> a032d7171d78843d6c174327468bd283310745f1
     </div>
   );
 }
