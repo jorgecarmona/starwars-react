f2204a48954d2ff866074ca15ca3f7c3784e3c58
Juan Carlos Reyes Meza <jcreyes@Juans-MacBook-Pro.local>
2023-02-21 19:02:46 -0500

    Added checkbox component again

---
+++ src/components/atoms/eng-checkbox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/App.css";
+
+import {ENGButton, ENGSwitch, ENGCheckbox} from "../components/atoms";
+
+function ENGCheckbox() {
+      <ENGCheckbox checked={true}/> 
+}
