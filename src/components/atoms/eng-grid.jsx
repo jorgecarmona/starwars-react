131fd65b8c0b9208fa27620d027c867ca3ae4ba6
Juan Carlos Reyes Meza <jcreyes@Juans-MacBook-Pro.local>
2023-02-23 13:47:44 -0500

    Added modified grid component

--- src/components/atoms/eng-grid.jsx
+++ src/components/atoms/eng-grid.jsx
@@ -12,6 +12,13 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
   
+  const Item2 = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#aaa',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   export default function BasicGrid() {
     return (
       <Box sx={{ flexGrow: 1 }}>
@@ -27,6 +34,27 @@ const Item = styled(Paper)(({ theme }) => ({
           </Grid>
           <Grid xs={8}>
             <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <Item2>xs=12</Item2>
+          </Grid>
+          <Grid xs={1}>
+            <Item2>xs=1</Item2>
+          </Grid>
+          <Grid xs={8}>
+            <Item2>xs=8</Item2>
+          </Grid>
+          <Grid xs={6}>
+            <Item2>xs=6</Item2>
+          </Grid>
+          <Grid xs={6}>
+            <Item2>xs=6</Item2>
+          </Grid>
+          <Grid xs={6}>
+            <Item2>xs=1</Item2>
           </Grid>
         </Grid>
       </Box>
